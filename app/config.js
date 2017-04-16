8af95550516146d550ffcb04c8cc4b0ca23cf818
J F <git@twopointzero.us>
2017-04-16 12:27:36 -0400

    Use PORT env variable from Glitch

--- app/config.js
+++ app/config.js
@@ -3,5 +3,5 @@ module.exports = {
   // Never check your production secret key into git
   key: '=rf#aayxoj-q9)q9z_ar5*=9_-&k+1y$fk9_0^!du+vm)!&mqz',
   server_interface: '127.0.0.1',
-  server_port: 8080,
+  server_port: process.env.PORT || 8080,
 };
